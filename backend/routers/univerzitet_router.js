ad1600262b39259d6df57b29e6c5097b0bfd53f3
Nemanja Dragisic <dragisic.nemanjaa@gmail.com>
2023-06-18 23:14:14 +0200

    Index za university controler

--- backend/routers/univerzitet_router.js
+++ backend/routers/univerzitet_router.js
@@ -1,8 +1,9 @@
-const pool = require("./../controlers/univerzitet_controler")
+/*
 const express = require("express")
 const router = express.Router()
 
 const univerzitetrouter = require("./../controlers/univerzitet_controler")
+const facultyrouter = require("./../controlers/fakultet_controler")
 router.route('/')
     .get(univerzitetrouter.getAllUniversities)
     .post(univerzitetrouter.insertUniveristy)
@@ -10,5 +11,7 @@ router.route('/:uniID')
     .get(univerzitetrouter.getUniversityByID)
 router.route('/:uniName')
     .get(univerzitetrouter.getUniversityByName)
+router.route('/:UniversityID/coleages')
+    .get(facultyrouter.getCollegeByUniversityID)
 
-module.exports = router
+module.exports = router*/
