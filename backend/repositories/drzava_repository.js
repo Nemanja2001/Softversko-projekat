eadc1de665763f078a4519788cfd728647d8df66
Nemanja Dragisic <dragisic.nemanjaa@gmail.com>
2023-06-18 12:42:05 +0200

    Promijenjena je struktura funkcija za upite kod Drzava repository jer prethodne nisu vracale kako treba sad su okej

--- backend/repositories/drzava_repository.js
+++ backend/repositories/drzava_repository.js
@@ -1,43 +1,31 @@
 const pool = require("./../dbconnection/dbconn");
 
-const getAllCountries = async ()=>{
-    return await pool.query('Select * from public."Drzava"',(error, results)=>{
-        if(error){
-            console.log("Greska pri selekciji drzava.", error);
-        }
-        return results.rows;
-    })
+const getAllCountries = async function (){
+    const res = await pool.query('Select * from public."Drzava"')
+    const rezultat = res.rows
+    return rezultat
 }
 
 const getCounrtyByName = async (name)=>{
-    return await pool.query('Select * from public."Drzava" where Ime = \'$1\'',[name], (error, results)=>{
-        if(error){
-            console.log('Ne postoji država sa ovim imenom.', error);
-        }
-        return results.rows
-    })
+    const result = await pool.query('Select * from public."Drzava" where "Ime" = $1',[name])
+    return result.rows
 }
 
 const insertCounry = async (name)=>{
-    return await pool.query('Insert into public."Drzava" (Ime) Values(\'$1\')',[name],(error, results)=>{
-        if(error)
-            console.log("Neuspješan unos!", error);
-        
-        return results.rows
-    })
+    const rezultat = await pool.query('Insert into public."Drzava" ("Ime") Values($1)',[name])
+    return rezultat.rows
 }
 
-const upadteCounry = async (name, newname)=>{
-    return await pool.query('Update public."Drzava" set Ime=\'$1\' where Ime=\'$2\')', [newname, name], (error, results) => {
-        if (error)
-            console.log("Neuspješan update!", error);
-        return results.rows;
-    })
+const updateCounry = async (name, newname)=>{
+    const result =await pool.query('Update public."Drzava" set "Ime"=$1 where "Ime"=$2', [newname, name])
+    res = result.rows
+    return res
 }
 
 module.exports = {
     getAllCountries,
     getCounrtyByName,
     insertCounry,
-    upadteCounry
+    updateCounry
 }
+
