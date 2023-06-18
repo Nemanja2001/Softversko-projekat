2086066c502f84b3d8f3b6279bdf7387ca309e96
Matija <matijaveljovic@yahoo.com>
2023-06-18 18:37:37 +0200

    Matkov komit

---
+++ backend/controlers/komentarfakultetskajedinica_controler.js
@@ -0,0 +1,54 @@
+const pool = require("./../dbconnection/dbconn");
+const comementfacultyUnitfunctions = require("./../repositories/komentarfakultetskajedinica_repository");
+
+const getAllFacultyUnitCommentar = async (request, response) => {
+  const results = await comementfacultyUnitfunctions.getAllFacultyUnitCommentar(
+    request.body
+  );
+  response.send(results);
+};
+
+const getUnityCommentByUserID = async (request, response) => {
+  const results = await comementfacultyUnitfunctions.getUnityCommentByUserID(
+    request.body.UserID
+  );
+  response.send(results);
+};
+
+const getGradeByCollege = async (request, response) => {
+  const results = await comementfacultyUnitfunctions.getGradeByCollege(
+    request.body
+  );
+  response.send(results);
+};
+
+const insertCommentCollegeUnity = async (request, response) => {
+  const results = await comementfacultyUnitfunctions.insertCommentCollegeUnity(
+    request.body
+  );
+  response.send(results);
+};
+
+const updateCommentCollegeUnity = async (request, response) => {
+  const results = await comementfacultyUnitfunctions.updateCommentCollegeUnity(
+    request.body
+  );
+  response.send(results);
+};
+
+const getNumberOfFacultyUnitComments = async (request, response) => {
+  const results =
+    await comementfacultyUnitfunctions.getNumberOfFacultyUnitComments(
+      request.body
+    );
+  response.send(results);
+};
+
+module.exports = {
+  getAllFacultyUnitCommentar,
+  getUnityCommentByUserID,
+  getGradeByCollege,
+  insertCommentCollegeUnity,
+  updateCommentCollegeUnity,
+  getNumberOfFacultyUnitComments,
+};
