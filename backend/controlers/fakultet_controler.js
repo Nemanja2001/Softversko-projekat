a85564af17c31356f34a77f4b0350911cbe95b0b
Nemanja Dragisic <dragisic.nemanjaa@gmail.com>
2023-06-18 17:29:12 +0200

    Na faculty repository imijenjeno user u faculty functions Dodati faculty controler i izmijenjen faculty repository

--- backend/controlers/fakultet_controler.js
+++ backend/controlers/fakultet_controler.js
@@ -1,27 +1,27 @@
 const { request, response } = require("express");
 const pool = require("./../dbconnection/dbconn")
-const userfunctions = require("./../repositories/fakultet_repository")
+const facultyfunctions = require("./../repositories/fakultet_repository")
 
 const getAllColleges = async(request, response)=>{
-    const result = await userfunctions.getAllColleges();
+    const result = await facultyfunctions.getAllColleges();
     response.send(result)
 }
 const getCollegeByUniversityID = async (request,response)=>{
     const UniversityID=request.params.UniversityID
-    const result=await userfunctions.getCollegeByUniversityID(UniversityID)
+    const result=await facultyfunctions.getCollegeByUniversityID(UniversityID)
     response.send(result)
     
 };
 const getCollegeByName = async (request,response)=>{
     const name=request.params.name
     const universityID=request.params.UniversityID
-    const result=await userfunctions.getCollegeByUniversityID(name,universityID)
+    const result=await facultyfunctions.getCollegeByUniversityID(name,universityID)
     response.send(result)
 };
 const insertCollege = async (request,response)=>{
     const universityID=request.params.UniversityID
     const name=request.params.name
-    const result=await userfunctions.insertCollege(universityID,name)
+    const result=await facultyfunctions.insertCollege(universityID,name)
     response.send(result)
 };
 module.exports={
--- backend/repositories/fakultetskajedinica_repository.js
+++ backend/repositories/fakultetskajedinica_repository.js
@@ -1,18 +1,19 @@
 const pool = require("../dbconnection/dbconn");
 
-const getFacultyUnitsByFaculty = async (FacultyName)=>{
-    const results = await pool.query('Select Ime from public."FakultetskaJedinica" where ImeFakulteta = $1',[FacultyName])
+const getFacultyUnitsByFaculty = async (FacultyName, UniversityID)=>{
+    const results = await pool.query('Select Ime from public."FakultetskaJedinica" where ImeFakulteta = $1 and IDUniverzitet=$2',
+                                    [FacultyName, UniversityID])
     return results.rows
 }
 
 const getFacultyUnitsByUniversity = async (UniversityID)=>{
-    const results = await pool.query('Select Ime from public."FakultetskaJedinica" where IDUniverziteta = $1',[UniversityID])
+    const results = await pool.query('Select Ime from public."FakultetskaJedinica" where IDUniverzitet = $1',[UniversityID])
     return results.rows
 }
 
-const insertFacultyUnit = async (UniversityID, FacultyName, Name)=>{
-    const results = await pool.query('Insert into public."FakultetskaJedinica" (IDUniverziteta, ImeFakulteta, Ime) Values($1, $2, $3)',
-                            [UniversityID,FacultyName,Name])
+const insertFacultyUnit = async (info)=>{
+    const results = await pool.query('Insert into public."FakultetskaJedinica" (IDUniverzitet, ImeFakulteta, Ime) Values($1, $2, $3)',
+                            [info.UniversityID,info.FacultyName,info.Name])
     return results.rows 
 }
 
