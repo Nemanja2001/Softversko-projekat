f0caab1802f56546440db2f18e69dce15b1bbf5e
Daniloz1 <daniloom63@gmail.com>
2023-06-20 01:06:59 +0200

    .

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,13 +3,13 @@ import logo from "./../photos/logo.jpg";
 import './Header.css'
 function Header(){
     return <div className="flex-conatiner">
-                <div><img src={logo} alt="Ovjde treba da bude logo" ></img></div>
-                <div id="div-flex">
+                <div><img id="logo" src={logo} alt="Ovjde treba da bude logo" ></img></div>
+                <div id="div-flex"> 
                     <div>Početna</div>
                     <div>O platformi</div>
                     <div>Studije</div>
                     <div>Aktivnosti</div>
-                    <div>Kontkat</div>
+                    <div>Kontakt</div>
                 </div>
                 <div id="logovanje">
                     <div>Registruj se</div>
