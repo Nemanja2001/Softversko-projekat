0da57ba2772bd6ad9c52b18642dfd9415d975bbd
Nemanja Dragisic <dragisic.nemanjaa@gmail.com>
2023-06-19 22:35:52 +0200

    Header

---
+++ frontend/src/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "./logo.jpg";
+import './Header.css'
+function Header(){
+    return <div className="flex-conatiner">
+                <div><img src={logo} alt="Ovjde treba da bude logo" ></img></div>
+                <div id="div-flex">
+                    <div>Početna</div>
+                    <div>O platformi</div>
+                    <div>Studije</div>
+                    <div>Aktivnosti</div>
+                    <div>Kontkat</div>
+                </div>
+                <div id="logovanje">
+                    <div>Registruj se</div>
+                    <div>Uloguj se</div>
+                </div>
+            </div>
+}
+
+export default Header
