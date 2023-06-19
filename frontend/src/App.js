57209461d12ac3cac8a4ada9af960d786f6d5c0d
lucijajovanovic22 <106869488+lucijajovanovic22@users.noreply.github.com>
2023-06-20 00:10:07 +0200

    kontakt stranica bez futer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import './App.css';
 import PocetnaStranica from './pages/Pocetna stranica';
+import Kontakt from './pages/Kontakt';
+
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path='/' exact Component={PocetnaStranica}></Route>
+          <Route path='/Kontakt' exact Component={Kontakt}></Route>
         </Routes>
       </Router>
     </>
---
+++ frontend/src/pages/Kontakt.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Header from "../components/Header"
+import './Kontakt.css'
+
+function Kontakt(){
+    return (<>
+        <Header/>
+        <div id="div1">
+        <p >Pošalji nam poruku</p>
+        <input type="text" placeholder="Ime i prezime"></input>
+        <br/>
+        <input type="email" placeholder="Email adresa"></input>
+        <br/>
+        <input type="text" placeholder="Tema"></input>
+        <br/>
+        <input  id="komentar" type="textarea" placeholder="Tekst poruke"></input>
+        <br/>
+        <input id="btnPosalji" type="submit" value="Pošalji"></input>
+        </div>
+        </>
+    )
+}
+export default Kontakt
