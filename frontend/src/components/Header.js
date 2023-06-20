ef8d4a9a047ba40ffc0ff916d03348c51c308c2e
Nemanja Dragisic <dragisic.nemanjaa@gmail.com>
2023-06-20 11:20:51 +0200

    Dodata provjera tokena radi rađenja testa profesionalne orijentacije

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,13 +2,23 @@ import React from "react";
 import logo from "./../photos/logo.jpg";
 import './Header.css'
 function Header(){
+
+    function provjera_tokena(){
+        console.log(localStorage.getItem('token'))
+        if(!localStorage.getItem('token')){
+            window.location='/login'
+        }else{
+            window.location='/testorijentacije'
+        }
+    }
+
     return <div className="flex-conatiner">
                 <div><img id="logo" src={logo} alt="Ovjde treba da bude logo" ></img></div>
                 <div id="div-flex"> 
                     <div onClick={()=>window.location='/'}>Početna</div>
                     <div>O platformi</div>
                     <div>Univerziteti</div>
-                    <div>Aktivnosti</div>
+                    <div onClick={provjera_tokena} style={{textAlign:'center'}}>Test profesionalne orijentacije</div>
                     <div onClick={()=>window.location='/kontakt'}>Kontakt</div>
                 </div>
                 <div id="logovanje">
