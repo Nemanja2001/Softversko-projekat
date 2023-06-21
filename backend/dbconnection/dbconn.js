41fc8a6e9f953e495160c48674829d822323d3fb
Nemanja Dragisic <dragisic.nemanjaa@gmail.com>
2023-06-21 22:15:50 +0200

    .

--- backend/dbconnection/dbconn.js
+++ backend/dbconnection/dbconn.js
@@ -4,11 +4,7 @@ const pool = new Pool({
   user: "postgres",
   host: "127.0.0.1",
   database: "KudaDalje_db",
-<<<<<<< HEAD
-  password: "220701",
-=======
-  password: "danilo",
->>>>>>> 6a985c1929cbbdc1f3e0062a3d539d87af4ee5c7
+  password: "nemanja",
   port: "5432",
 });
 
