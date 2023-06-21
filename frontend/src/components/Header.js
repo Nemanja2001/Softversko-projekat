a027f7f07cbb654cfd5d65ddd7f6d00c205f60c5
Nemanja Dragisic <dragisic.nemanjaa@gmail.com>
2023-06-21 13:22:13 +0200

    malo izmijenjen header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,43 +6,52 @@ import UlogujSe from "./UlogujSe";
 import UlogovanSam from "./UlogovanSam";
 function Header(){
     let location = useLocation()
-    function provjera_tokena(){
+    function promjena_na_test_orijentacije(){
         console.log(localStorage.getItem('token'))
         if(!localStorage.getItem('token')){
             window.location='/login'
         }else{
-            window.location='/testorijentacije'
+            window.location='/korisnik/'+localStorage.getItem('username')+'/testorijentacije'
         }
     }
+
+    function promjena_na_pocetnu(){
+        if(!localStorage.getItem('token'))
+            window.location='/'
+        else
+            window.location='/korisnik/'+localStorage.getItem('username')
+    }
+
+    function promjena_na_oplatformi(){
+        if(!localStorage.getItem('token'))
+            window.location='/Oplatformi'
+        else
+            window.location = '/korisnik/'+localStorage.getItem('username')+'/Oplatformi'
+    }
+
+    function promjena_na_univerziteti(){
+        if(!localStorage.getItem('token'))
+            window.location='/univerziteti'
+        else
+            window.location ='/korisnik/'+localStorage.getItem('username')+'/univerziteti'
+    }
+
+    function promjena_na_kontkt(){
+        if(!localStorage.getItem('token'))
+            window.location='/kontakt'
+        else
+            window.location='/korisnik/'+localStorage.getItem('username')+'/kontakt'
+    }
+    
 
     return <div className="flex-conatiner">
                 <div><img id="logo" src={logo} alt="Ovjde treba da bude logo" ></img></div>
                 <div id="div-flex"> 
-                    <div onClick={()=>{
-                        if(!localStorage.getItem('token'))
-                            window.location='/'
-                        else
-                            window.location='/korisnik/'+localStorage.getItem('username')
-                    }}>Početna</div>
-                    <div onClick={()=>{
-                        if(!localStorage.getItem('token'))
-                            window.location='/Oplatformi'
-                        else
-                            window.location = '/korisnik/'+localStorage.getItem('username')+'/Oplatformi'
-                        }}>O platformi</div>
-                    <div onClick={()=>{
-                        if(!localStorage.getItem('token'))
-                            window.location='/univerziteti'
-                        else
-                            window.location ='/korisnik/'+localStorage.getItem('username')+'/univerziteti'
-                        }}>Univerziteti</div>
-                    <div onClick={provjera_tokena} style={{textAlign:'center'}}>Test profesionalne orijentacije</div>
-                    <div onClick={()=>{
-                        if(!localStorage.getItem('token'))
-                            window.location='/kontakt'
-                        else
-                            window.location='/korisnik/'+localStorage.getItem('username')+'/kontakt'
-                    }}>Kontakt</div>
+                    <div onClick={promjena_na_pocetnu}>Početna</div>
+                    <div onClick={promjena_na_oplatformi}>O platformi</div>
+                    <div onClick={promjena_na_univerziteti}>Univerziteti</div>
+                    <div onClick={promjena_na_test_orijentacije} style={{textAlign:'center'}}>Test profesionalne orijentacije</div>
+                    <div onClick={promjena_na_kontkt}>Kontakt</div>
                 </div>
                 {!location.pathname.includes('korisnik') && <UlogujSe/>}
                 {location.pathname.includes('korisnik') && <UlogovanSam/>}
