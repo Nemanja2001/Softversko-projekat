b01127119b0e3dbea68e40d842a588706150f3be
lucijajovanovic22 <106869488+lucijajovanovic22@users.noreply.github.com>
2023-06-21 21:01:37 +0200

    .

---
+++ frontend/src/components/GlobalnePromjenljive.js
@@ -0,0 +1,9 @@
+let globalnaPromenljiva = 0;
+
+export const getGlobalnaPromenljiva = () => {
+  return globalnaPromenljiva;
+};
+
+export const setGlobalnaPromenljiva = (novaVrednost) => {
+  globalnaPromenljiva = novaVrednost;
+};
