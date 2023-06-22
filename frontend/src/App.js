429c07731d63080dd8e98809475cef23f99ff079
Nemanja Dragisic <dragisic.nemanjaa@gmail.com>
2023-06-23 01:34:56 +0200

    zakljucane rute za ulogovane korisnike

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,24 +20,23 @@ function App() {
     <Router>
         <Routes>
           <Route path='/' exact Component={PocetnaStranica}></Route>
-          <Route path='/korisnik/:userID' Component={PocetnaStranica}></Route>
+          {localStorage.getItem('token') && <Route path='/korisnik/:userID' Component={PocetnaStranica}></Route>}
           <Route path='/Kontakt' exact Component={Kontakt}></Route>
-          <Route path='/korisnik/:userID/kontakt' Component={Kontakt}></Route>
+          {localStorage.getItem('token') && <Route path='/korisnik/:userID/kontakt' Component={Kontakt}></Route>}
           <Route path='/login' exact Component={Login}></Route>
-          <Route path='/korisnik/:userID' Component={PocetnaStranica}></Route>
           <Route path='/Oplatformi' Component={Oplatformi}></Route>
-          <Route path='/korisnik/:userID/testorijentacije' Component={TestOrijentacijePocetna}></Route>
-          <Route path='/korisnik/:userID/testorijentacije/Test' Component={Test}></Route>
-          <Route path='/korisnik/:userID/testorijentacije/Test2' Component={Test2}></Route>
-          <Route path='/korisnik/:userID/Oplatformi' Component={Oplatformi}></Route>
+          {localStorage.getItem('token') && <Route path='/korisnik/:userID/testorijentacije' Component={TestOrijentacijePocetna}></Route>}
+          {localStorage.getItem('token') && <Route path='/korisnik/:userID/testorijentacije/Test' Component={Test}></Route>}
+          {localStorage.getItem('token') && <Route path='/korisnik/:userID/testorijentacije/Test2' Component={Test2}></Route>}
+          {localStorage.getItem('token') && <Route path='/korisnik/:userID/Oplatformi' Component={Oplatformi}></Route>}
           <Route path='/univerziteti' Component={Univerziteti}></Route>
-          <Route path='/korisnik/:userID/univerziteti' Component={Univerziteti}></Route>
-          <Route path='/korisnik/:userID/testorijentacije/RezultatiTesta' Component={RezultatiTesta}></Route>
+          {localStorage.getItem('token') && <Route path='/korisnik/:userID/univerziteti' Component={Univerziteti}></Route>}
+          {localStorage.getItem('token') && <Route path='/korisnik/:userID/testorijentacije/RezultatiTesta' Component={RezultatiTesta}></Route>}
           <Route path='/registracija' Component={Register}></Route>
           <Route path='/univerziteti/:uniID/fakulteti' Component={Fakulteti}></Route>
-          <Route path='/korisnik/:userID/univerziteti/:uniID/fakulteti' Component={Fakulteti}></Route>
+          {localStorage.getItem('token') && <Route path='/korisnik/:userID/univerziteti/:uniID/fakulteti' Component={Fakulteti}></Route>}
           <Route path='/univerziteti/:uniID/fakulteti/:facultyName/fakultetskejedinice' Component={FakultetskaJednicia}></Route>
-          <Route path='/korisnik/:userID/univerziteti/:uniID/fakulteti/:facultyName/fakultetskejedinice' Component={FakultetskaJednicia}></Route>
+          {localStorage.getItem('token') && <Route path='/korisnik/:userID/univerziteti/:uniID/fakulteti/:facultyName/fakultetskejedinice' Component={FakultetskaJednicia}></Route>}
       </Routes>
     </Router>
     </>
