94a641f28f80a063b9198815a16fde43e9e6619b
Nemanja Dragisic <dragisic.nemanjaa@gmail.com>
2023-06-23 13:38:18 +0200

    Samo mala izmjena na pocetnoj

--- frontend/src/pages/Pocetna stranica.js
+++ frontend/src/pages/Pocetna stranica.js
@@ -3,7 +3,19 @@ import Header from "../components/Header"
 import Photo from "../components/Photo"
 import Text from "../components/Text"
 import Footer from "../components/Footer"
+import { useLocation } from "react-router-dom"
 function PocetnaStranica(){
+    let location = useLocation()
+    function provjera(){
+        if(!location.pathname.includes('korisnik') && !location.pathname.includes('admin')){
+            localStorage.removeItem('token');
+            localStorage.removeItem('admin');
+            localStorage.removeItem('username');
+        }
+    }
+
+    provjera();
+
     return(<>
         <Header/>
         <Photo/>
