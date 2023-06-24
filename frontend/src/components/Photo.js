74a9d4427ef0eb135e96bb629382d1cf7bdbdd25
Matija <matijaveljovic@yahoo.com>
2023-06-24 17:02:42 +0200

    Dzaba ste krecili

--- frontend/src/components/Photo.js
+++ frontend/src/components/Photo.js
@@ -1,19 +1,25 @@
 import React from "react";
-import './Photo.css'
-import slika from './../photos/masan.jpg'
-function Photo(){
-    return <div>
-            <div id="div1">
-                <img id="slika1"src={slika} alt="Ovdje ide Masanova slika">
-            </img>
-            </div>
-            <div id="div2">
-                <h1 id="Naslov1">Kuda dalje</h1>
-                <hr></hr>
-                <h3>Jedino ograničenje našem dostignuću je naša sposobnost da tačno odredimo ono što želimo. – Entoni Robins</h3>
-                <button class="btn" onClick={()=>window.location='/univerziteti'} >Univerziteti</button>
-            </div>    
-            </div>
+import "./Photo.css";
+import slika from "./../photos/masan2.jpg";
+function Photo() {
+  return (
+    <div>
+      <div id="div1">
+        <img id="slika1" src={slika} alt="Ovdje ide Masanova slika"></img>
+      </div>
+      <div id="div2">
+        <p id="Naslov1">Kuda dalje</p>
+        <hr></hr>
+        <h3>
+          Jedino ograničenje našem dostignuću je naša sposobnost da tačno
+          odredimo ono što želimo. – Entoni Robins
+        </h3>
+        <button class="btn" onClick={() => (window.location = "/univerziteti")}>
+          Univerziteti
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Photo
+export default Photo;
--- frontend/src/components/Text.js
+++ frontend/src/components/Text.js
@@ -1,22 +1,31 @@
 import React from "react";
-import './Text.css'
-function Text(){
-    const predifinedtext ="Korak više u \n daljem obrazovanju";
-    const text1 ="Svjesni činjenice da su maturanti pretežno djeca uzrasta od 18 do\n 19 godina i da su samim tim tek na početku formiranja svoje ličnosti i svoje\n karijere, često su vrlo neodlučni i odabir budućeg\n zanimanja kroz odabir fakulteta, za njih najčešće predstavlja veliki\n izazov. Odabir fakulteta, sam po sebi, predstavlja prekretnicu u\n životima mladih ljudi zbog čega je još jasnija neodlučnost\n maturanata kada je ovako važna odluka u pitanju. Podrazumijeva\n se da je to odluka koju će svi oni samostalno morati da donesu, ali\n kako su mladi ljudi budućnost društva, nastojimo da im pomoću\n ovog servisa bar malo olakšamo."
-    const text2 ="Našu platformu vidimo kao jedan od načina da  srednjoškolci\n starijih razreda na vrijeme upoznaju mogućnosti na našim\n fakultetima u državi, da vide kakva su iskustva ljudi sa njihovog\n željenog fakulteta i da mogu da postave određeno pitanje na koje \nostali mogu da vide odgovor, ako imaju istu ili sličnu nedoumicu."
-    
-    return <div id="div">
-        <div><p id="paragraph1" style={{whiteSpace:'pre-line'}}>{predifinedtext}</p></div>
-        <div id="div3-flex">
-            <div>
-                {text1}
-            </div>
-            <div>
-                {text2}
-            </div>
-        </div>
-        <br/>
+import "./Text.css";
+import video from "./../photos/video.mp4";
+import slika from "./../photos/DrNemanjaPrvi.jpg";
+function Text() {
+  const predifinedtext = "Korak više u \n daljem obrazovanju";
+  const text1 =
+    "Svjesni činjenice da su maturanti pretežno djeca uzrasta od 18 do 19 godina\n i da su samim tim tek na početku formiranja svoje ličnosti i svoje karijere,\n često su vrlo neodlučni i odabir budućeg zanimanja kroz odabir fakulteta, \nza njih najčešće predstavlja veliki izazov. Odabir fakulteta, sam po sebi, \npredstavlja prekretnicu u životima mladih ljudi zbog čega je još jasnija\n neodlučnost maturanata kada je ovako važna odluka u pitanju. \nPodrazumijeva se da je to odluka koju će svi oni samostalno morati \nda donesu, ali kako su mladi ljudi budućnost društva, nastojimo \nda im pomoću ovog servisa bar malo olakšamo.";
+  const text2 =
+    "Našu platformu vidimo kao jedan od načina da  srednjoškolci\n starijih razreda na vrijeme upoznaju mogućnosti na našim\n fakultetima u državi, da vide kakva su iskustva ljudi sa njihovog\n željenog fakulteta i da mogu da postave određeno pitanje na koje \nostali mogu da vide odgovor, ako imaju istu ili sličnu nedoumicu.";
+
+  return (
+    <div id="div">
+      <div>
+        <p id="paragraph1" style={{ whiteSpace: "pre-line" }}>
+          {predifinedtext}
+        </p>
+      </div>
+      <div id="div3-flex">
+        <div>{text1}</div>
+        <div>{text2}</div>
+      </div>
+      <br />
+      <video src={video} controls poster={slika}></video>
+      <br />
+      <br />
     </div>
+  );
 }
 
-export default Text
+export default Text;
